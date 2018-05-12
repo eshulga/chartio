a645bb12df2698f5a6e88388eb8017dbc12d97b4
Sergei M <9957040@gmail.com>
2018-05-12 15:05:37 +0300

    Button js to pdf

---
+++ src/common/download-button/DownloadButton.vue
@@ -0,0 +1,57 @@
+<template>
+    <button @click='jsToPdf'><img src="../../assets/img/download-btn-icon.png" alt="">Download PDF</button>
+    
+</template>
+
+<script>
+import jsPDF from 'jsPDF'
+
+export default {
+    name: 'DownloadButton',
+    data(){
+        return{
+            
+        }
+    },
+    methods:{
+        jsToPdf(){
+            let pdfName = 'test'; 
+            var doc = new jsPDF();
+            doc.text("Hello World!", 10, 10);
+            doc.save(pdfName + '.pdf');
+            console.log("CLICK!")
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+button{
+    width: 567px;
+    height: 168px;
+    border-radius: 20px;
+    background-color: #ff5722;
+    font-family: Roboto;
+    color:  #ffffff;
+    font-size: 50px;
+    font-weight: 400;
+    line-height: 14px;
+    text-align: left;
+}
+img{
+    margin: 10px 20px 10px 10px;
+}
+
+
+</style>
--- src/components/table-area/TableArea.vue
+++ src/components/table-area/TableArea.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="main">
     <h1>Table-area</h1>
+    <download-button/>
   </div>
 </template>
 
 <script>
+import DownloadButton from '../../common/download-button/DownloadButton.vue'
 export default {
   name: 'TableArea',
   data () {
     return {
     }
+  },
+  components:{
+  	DownloadButton
   }
 }
 </script>
