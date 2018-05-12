999035ad4fead25ef9730efb18bbe5a232d6d90e
Alex <alex.guzey@gmail.com>
2018-05-12 19:23:40 +0300

    merge

--- src/common/color-picker/ColorPicker.vue
+++ src/common/color-picker/ColorPicker.vue
@@ -1,11 +1,6 @@
 <template>
-<<<<<<< HEAD
   <div class="area">
     <div class="area__item" :class="activeItem === index ? 'area__item-active' : ''" v-for="(color, index) in colors" :key="index" :style="'background-color: #' + color" @click="onClick(color, index)">
-=======
-    <div class="area">
-      <div class="area__item" v-for="color in colors" :key="color.id" :style="'background-color: #' + color"></div>
->>>>>>> dev
     </div>
   </div>
 </template>
@@ -15,7 +10,6 @@ export default {
   name: 'ColorPicker',
   data () {
     return {
-<<<<<<< HEAD
       colors: ['f70505', 'fff200', '00a651', '00aef0', '9e1f63', 'ec008c', 'be1e2d', 'ef4136', 'f15a29', 'f7941d', 'fbb040', 'd7df23', '8dc63f', '39b54a', '009444', '006838', '2bb673', '00a79d', '27aae1', '1b75bc', '2b3990', '262262', '662d91', '92278f'],
       activeItem: null
     }
@@ -24,9 +18,6 @@ export default {
     onClick (color, index) {
       this.$root.$emit('close-color-picker', `#${color}`)
       this.activeItem = index
-=======
-      // colors: ["ed1c24", "fff200", "00a651", "00aef0", "9e1f63", "ec008c", "be1e2d", "ef4136", "f15a29", "f7941d", "fbb040", "d7df23", "8dc63f", "39b54a", "009444", "006838", "2bb673", "00a79d", "27aae1", "1b75bc", "2b3990", "262262", "662d91", "92278f"]
->>>>>>> dev
     }
   }
 }
