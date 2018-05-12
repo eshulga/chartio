ef535f08ab664653ff345ecde7497e13462f6aa2
Evgenii Shulga <ewgenij.shulga@gmail.com>
2018-05-12 14:24:17 +0300

    add xlsx files reader and parsing

--- src/components/table-area/TableArea.vue
+++ src/components/table-area/TableArea.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="main">
     <h1>Table-area</h1>
+    <input type="file" name="xlfile" id="xlf" @change="fileInputHandler">
   </div>
 </template>
 
 <script>
+import XLSX from 'xlsx'
+
 export default {
   name: 'TableArea',
   data () {
     return {
+      files: []
+    }
+  },
+  methods: {
+    fileInputHandler (e) {
+      console.dir(e.target.files)
+      let data = new Uint8Array(e.target.files)
+      console.dir(data)
+      var files = e.target.files
+      for (let i = 0, f = files[i]; i !== files.length; ++i) {
+        var reader = new FileReader()
+        var name = f.name
+        reader.onload = function (e) {
+          var data = e.target.result
+          var workbook = XLSX.read(data, {type: 'binary'})
+          console.log(name, workbook)
+        }
+        reader.readAsBinaryString(f)
+      }
     }
   }
 }
