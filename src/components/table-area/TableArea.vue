7d3187851e3034103a082d4438e0d4d00ab7f213
Unknown <onipko32@gmail.com>
2018-05-12 13:02:07 +0300

    Added fixes in layout

--- src/components/table-area/TableArea.vue
+++ src/components/table-area/TableArea.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="main">
-
   </div>
 </template>
 
 <script>
 export default {
   name: 'TableArea',
-  data() {
-      return {
-
-      }
+  data () {
+    return {
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
-
