2cf223f75711e76b4a68a164172fdb67b21763be
Alex <alex.guzey@gmail.com>
2018-05-12 18:40:53 +0300

    commit

--- src/common/color-picker/ColorPicker.vue
+++ src/common/color-picker/ColorPicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="area">
-    <div class="area__item" v-for="color in colors" :key="color.id" :style="'background-color: #' + color" @click="onClick(color)">
-      <div class="area__item_shadow" v-if="activeItem[colors.indexOf(color)]"></div>
+    <div class="area__item" v-for="color in colors" :key="color.id" :style="'background-color: #' + color" @click="onClick(color)" :activeItem="activeItem[colors.indexOf(color)] = false">
+      <div class="area__item_shadow" v-if="activeItem[colors.indexOf(item)]"></div>
     </div>
   </div>
 </template>
@@ -12,25 +12,22 @@ export default {
   data () {
     return {
       colors: ['f70505', 'fff200', '00a651', '00aef0', '9e1f63', 'ec008c', 'be1e2d', 'ef4136', 'f15a29', 'f7941d', 'fbb040', 'd7df23', '8dc63f', '39b54a', '009444', '006838', '2bb673', '00a79d', '27aae1', '1b75bc', '2b3990', '262262', '662d91', '92278f'],
-      activeItem: []
+      activeItem: [],
+      currentActice: ''
     }
   },
   methods: {
     onClick (item) {
       this.$root.$emit('close-color-picker', `#${item}`)
       const keyItem = this.colors.indexOf(item)
-      const activeItems = this.activeItem;
-      activeItems[keyItem] = true;
-      this.activeItem = activeItems;
+      this.activeItem[keyItem] = true;
+      console.log('onClick', this.activeItem)
     }
   },
   watch: {
-    activeItemWatch () {
+    activeItem () {
       console.log('watch')
     }
-  },
-  computed () {
-    this.activeItemWatch = this.activeItem
   }
 }
 </script>
