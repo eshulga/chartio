aa6d15728760ef69a3771b2cf8b38ea64af91346
Evgenii Shulga <ewgenij.shulga@gmail.com>
2018-05-12 12:50:10 +0300

    table area & controls crete

---
+++ src/components/controls/controls.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+<script>
+
+import TableArea from '../table-area/TableArea.vue'
+
+export default {
+  name: 'Controls',
+  data () {
+      return {
+
+      }
+  },
+  components: {
+      TableArea
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
